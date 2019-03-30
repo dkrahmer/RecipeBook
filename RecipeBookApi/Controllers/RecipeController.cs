15fd63641c1570591b5f095a4a8f7b8c99de6491
Andy Ladd <aladd04@gmail.com>
2019-03-30 14:22:08 -0400

    Removed the test controller, added the recipe controller. Only anonymous get actions, for now.

---
+++ RecipeBookApi/Controllers/RecipeController.cs
@@ -0,0 +1,44 @@
+﻿using Common.Dynamo.Contracts;
+using Common.Dynamo.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace RecipeBookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IDynamoStorageRepository<Recipe> _recipeStorage;
+
+        public RecipeController(IDynamoStorageRepository<Recipe> recipeStorage)
+        {
+            _recipeStorage = recipeStorage;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllRecipesAsync()
+        {
+            var recipes = await _recipeStorage.ReadAll();
+
+            return Ok(recipes);
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("{recipeId}")]
+        public async Task<IActionResult> GetRecipeByIdAsync(string recipeId)
+        {
+            var recipe = await _recipeStorage.Read(recipeId);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(recipe);
+        }
+    }
+}
