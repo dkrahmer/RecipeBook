cfec779d9507fc8d685ba0e7a0d7019c4c8869a0
Andy Ladd <aladd04@gmail.com>
2019-03-30 00:15:25 -0400

    Removed S3. Added Swagger.

--- RecipeBookApi/Startup.cs
+++ RecipeBookApi/Startup.cs
@@ -1,28 +1,29 @@
-using Amazon.S3;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace RecipeBookApi
 {
     public class Startup
     {
-        public const string AppS3BucketKey = "AppS3Bucket";
+        public static IConfiguration Configuration { get; private set; }
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public static IConfiguration Configuration { get; private set; }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddAWSService<IAmazonS3>();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info { Title = "Recipe Book API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -35,6 +36,12 @@ namespace RecipeBookApi
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "Recipe Book API");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
