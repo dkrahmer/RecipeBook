fdcf6f7e89fef27bcc7752a60c66a3fa3081d562
Doug Krahmer <doug.git@remhark.com>
2019-05-21 09:57:45 -0700

    Minor model and style updates

--- src/Routes/RecipeView/Components/RecipeInfo.js
+++ src/Routes/RecipeView/Components/RecipeInfo.js
@@ -2,40 +2,43 @@ import { RecipeInfoSection } from "./RecipeInfoSection";
 import React from "react";
 
 export function RecipeInfo({ recipe, ...props }) {
-  props.setOwnerBlurb(generateOwnerBlurb(recipe.ownerName, recipe.updateDate));
-
+  props.setOwnerBlurb(generateOwnerBlurb(recipe.ownerName, recipe.updateDateTime));
+  
   return (
     <React.Fragment>
-      <RecipeInfoSection
-        title="Description"
-        body={recipe.description} />
       <RecipeInfoSection
         title="Ingredients"
         body={recipe.ingredients} />
       <RecipeInfoSection
         title="Instructions"
         body={recipe.instructions} />
+      <RecipeInfoSection
+        title="Description"
+        body={recipe.description} />
+      <RecipeInfoSection
+        title="Notes"
+        body={recipe.notes} />
     </React.Fragment>
   );
 }
 
-function generateOwnerBlurb(ownerName, updateDate) {
+function generateOwnerBlurb(ownerName, updateDateTime) {
   const dateString = new Intl.DateTimeFormat("en-US", {
-    timeZone: "America/New_York",
+    timeZone: "UTC",
     year: "numeric",
     month: "long",
     day: "numeric",
     weekday: "long"
-  }).format(new Date(updateDate));
+  }).format(new Date(updateDateTime));
 
   const timeString = new Intl.DateTimeFormat("en-US", {
-    timeZone: "America/New_York",
+    timeZone: "UTC",
     hour: "numeric",
     minute: "numeric",
     hour12: "true"
-  }).format(new Date(updateDate));
+  }).format(new Date(updateDateTime));
 
   const dateTimeString = `${dateString} at ${timeString}`;
 
-  return `${ownerName} last updated this recipe on ${dateTimeString}`;
+  return `Last update: ${dateTimeString}`;
 }
--- src/Routes/RecipesGrid/Components/RecipeGridCard.js
+++ src/Routes/RecipesGrid/Components/RecipeGridCard.js
@@ -11,33 +11,23 @@ import {
 
 export function RecipeGridCard({ recipe }) {
   const dateString = new Intl.DateTimeFormat("en-US", {
-    timeZone: "America/New_York",
+    timeZone: "UTC",
     year: "numeric",
     month: "long",
     day: "numeric",
     hour: "numeric",
     minute: "numeric",
     hour12: "true"
-  }).format(new Date(recipe.updateDate));
+  }).format(new Date(recipe.updateDateTime));
 
   return (
     <React.Fragment>
       <Card>
-        <CardHeader
-          title={recipe.name}
-          subheader={`${recipe.ownerName} - ${dateString}`} />
-        <CardContent>
-          <Typography variant="body1">
-            {recipe.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <RouterLink to={`/recipe/${recipe.id}`}>
-            <Button size="small" color="primary">
-              See More
-            </Button>
-          </RouterLink>
-        </CardActions>
+        <RouterLink to={`/recipe/${recipe.recipeId}`}>
+		  <CardHeader
+            title={recipe.name}
+            subheader={`${dateString}`} />
+        </RouterLink>
       </Card>
     </React.Fragment>
   );
