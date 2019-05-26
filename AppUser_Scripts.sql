fd50026e2091ddc878b0e0ba5f25ebb7015ced5c
Doug Krahmer <doug.git@remhark.com>
2019-05-25 23:52:19 -0700

    Add MySql schema and AppUser scripts

---
+++ AppUser_Scripts.sql
@@ -0,0 +1,14 @@
+﻿# AppUserId, Username, FirstName, LastName, CanViewRecipe, CanEditRecipe, IsAdmin, LastLoggedInDate, CreateDateTime, UpdateDateTime
+
+-- Add a new user that has never logged in before
+INSERT INTO AppUsers (Username, FirstName, LastName, CanViewRecipe, CanEditRecipe, IsAdmin)
+VALUES ('______@gmail.com', '<First>', '<Last>', 1, 1, 1);
+
+
+-- Give permissions to an existing user
+UPDATE AppUsers
+SET
+	CanViewRecipe = 1,
+	CanEditRecipe = 1,
+	IsAdmin = 1
+WHERE Username = '______@gmail.com'
