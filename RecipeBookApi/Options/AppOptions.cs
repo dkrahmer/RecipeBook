6d5dfa15aa8c5c96414be36c195e6751dbac1947
Doug Krahmer <doug.git@remhark.com>
2020-10-10 16:58:09 -0700

    Change AllowedOrigins to string array.

--- RecipeBookApi/Options/AppOptions.cs
+++ RecipeBookApi/Options/AppOptions.cs
@@ -6,7 +6,7 @@ namespace RecipeBookApi.Options
 {
 	public class AppOptions
 	{
-		public string AllowedOrigins { get; set; }
+		public string[] AllowedOrigins { get; set; }
 		public string GoogleClientId { get; set; }
 		public string GoogleClientSecret { get; set; }
 		public string MySqlConnectionString { get; set; }
