0992f8d05d2c59d18d376eda834350f002a825e1
Doug Krahmer <doug.git@remhark.com>
2022-07-29 16:47:28 -0700

    Remote recipe scraper API

---
+++ RecipeScraperApi/app.js
@@ -0,0 +1,54 @@
+import express from "express";
+import http from "http";
+import recipeDataScraper from "recipe-data-scraper";
+
+async function recipeScraperApi() {
+	const app = express();
+	const server = http.createServer(app);
+
+	app.get("/", async function (req, res) {
+		const recipeUrl = req.query.url;
+
+		try {
+			console.log(`Recipe URL: ${recipeUrl}`);
+
+			// https://www.npmjs.com/package/recipe-data-scraper
+			const scrapedRecipe = await recipeDataScraper(recipeUrl);
+
+			let notes = `Based on: ${recipeUrl}`
+
+			if (scrapedRecipe.totalTime)
+				notes = `Cook time: ${scrapedRecipe.cookTime}\n` + notes;
+
+			if (scrapedRecipe.totalTime)
+				notes = `Prep time: ${scrapedRecipe.prepTime}\n` + notes;
+
+			if (scrapedRecipe.totalTime)
+				notes = `Total time: ${scrapedRecipe.totalTime}\n` + notes;
+
+			if (scrapedRecipe.recipeYield)
+				notes = `Yield: ${scrapedRecipe.recipeYield.replace(/([0-9]+)/g, "<$1>")}\n` + notes;
+
+			const recipe = {
+				"Name": scrapedRecipe.name,
+				"Ingredients": scrapedRecipe.recipeIngredients.join("\n"),
+				"Instructions": scrapedRecipe.recipeInstructions.join("\n"),
+				"Tags": scrapedRecipe.recipeCategories,
+				"Notes": notes,
+				"ImageUrls": [ scrapedRecipe.image ]
+			};
+
+			res.json(recipe);
+		} catch (ex) {
+			console.log(`GET error: ${ex.message}`);
+			res.status(500).json({ message: ex.message });
+		}
+	});
+
+	server.listen(5091, "localhost");
+	server.on("listening", function () {
+		console.log("Express server started: http://%s:%s", server.address().address, server.address().port);
+	});
+}
+
+export default recipeScraperApi();
